81d505f91c3c43033ee8f3e51adcf58d3f4812df
Sunil.Pandey <Sunil.Pandey@DESKTOP-M1K358G>
2019-03-01 21:34:06 +0530

    Excell Sheet

--- SGS_TEST_SYSTEM_ConsoleAPP/Program.cs
+++ SGS_TEST_SYSTEM_ConsoleAPP/Program.cs
@@ -7,28 +7,60 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace SGS_TEST_SYSTEM_ConsoleAPP
 {
-   
+
     class Program
     {
-        public static int rowCount ;
-        public static int colCount ;
+        public static int rowCount;
+        public static int colCount;
         public static Dictionary<string, string> SheetReturValues = new Dictionary<string, string>();
         public static Dictionary<string, int> SheetReturCount = new Dictionary<string, int>();
         public static Excel.Range xlRange;
-        public static int loopCount =  1;
+        public static int loopCount = 1;
+        public static string ledOnCode;
+        public static string ledOffCode;
+        public static string trayCode;
+        public static string sWInput;
+        public static string switchCode;
         static void Main(string[] args)
         {
-            ReadExcelSheet();
-            InitailizeExelSheet();
-            SheetReturValues.Add("j", Convert.ToString(1));
-            SheetReturValues.Add("i", Convert.ToString(1));
-            while (true)
+            //ReadExcelSheet();
+            //InitailizeExelSheet();
+            //SheetReturValues.Add("j", Convert.ToString(1));
+            //SheetReturValues.Add("i", Convert.ToString(1));
+            //while (true)
+            //{
+
+            //    ExelSheetValues(Convert.ToInt32(SheetReturValues["j"]), Convert.ToInt32(SheetReturValues["i"]));
+            //    SheetReturValues["i"] = Convert.ToString(Convert.ToInt32(SheetReturValues["i"])+1);
+            //}
+            //  Console.ReadLine();Convert
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Sunil.Pandey\Desktop\CABLETESTSHEET1\CABLETESTSHEET1\sheet1.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            for (int i = 2; i < 12; i++)
             {
-               
-                ExelSheetValues(Convert.ToInt32(SheetReturValues["j"]), Convert.ToInt32(SheetReturValues["i"]));
-                SheetReturValues["i"] = Convert.ToString(Convert.ToInt32(SheetReturValues["i"])+1);
+                if (xlRange.Cells[i, 2] != null && xlRange.Cells[i, 2].Value2 != null)
+                {
+                    
+                    trayCode = xlRange.Cells[i, 2].Value2.ToString().Trim('/');
+                    string temp1 = xlRange.Cells[i, 3].Value2.ToString().Trim('/');
+                    string temp2 = xlRange.Cells[i, 5].Value2.ToString().Trim('/');
+                    ledOnCode = trayCode + temp1;
+                    ledOffCode = trayCode + temp2;
+                    switchCode = xlRange.Cells[i, 4].Value2.ToString().Trim('/');
+                    sWInput = SwitchPress();
+                    
+                    Console.WriteLine(switchCode);
+                    Console.WriteLine(ledOnCode);
+                    if (sWInput == switchCode)
+                    {
+                        Console.WriteLine(ledOffCode);
+                    }
+
+
+                }
             }
-          //  Console.ReadLine();Convert
         }
         private static void ReadExcelSheet()
         {
@@ -39,8 +71,8 @@ namespace SGS_TEST_SYSTEM_ConsoleAPP
                 Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Sunil.Pandey\Desktop\CABLETESTSHEET1\CABLETESTSHEET1\sheet1.xlsx");
                 Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
                 Excel.Range xlRange = xlWorksheet.UsedRange;
-                 rowCount = xlRange.Rows.Count;
-                 colCount = xlRange.Columns.Count;
+                rowCount = xlRange.Rows.Count;
+                colCount = xlRange.Columns.Count;
                 for (int i = 1; i <= rowCount; i++)
                 {
                     for (int j = 1; j <= colCount; j++)
@@ -51,10 +83,10 @@ namespace SGS_TEST_SYSTEM_ConsoleAPP
 
                         //write the value to the console
                         if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                            
+
                         {
                             Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-                            
+
                         }
                     }
                 }
@@ -73,19 +105,21 @@ namespace SGS_TEST_SYSTEM_ConsoleAPP
 
         }
 
-        public static Dictionary<string,int> InitailizeExelSheet() {
+        public static Dictionary<string, int> InitailizeExelSheet()
+        {
             Excel.Application xlApp = new Excel.Application();
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Sunil.Pandey\Desktop\CABLETESTSHEET1\CABLETESTSHEET1\sheet1.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-             xlRange = xlWorksheet.UsedRange;
+            xlRange = xlWorksheet.UsedRange;
             rowCount = xlRange.Rows.Count;
             colCount = xlRange.Columns.Count;
             SheetReturCount.Add("rowCount", rowCount);
             SheetReturCount.Add("colCount", colCount);
             return SheetReturCount;
         }
-        public static Dictionary<string, string> ExelSheetValues(int j,int i) {
-           
+        public static Dictionary<string, string> ExelSheetValues(int j, int i)
+        {
+
             if (j == 1)
                 Console.Write("\r\n");
 
@@ -93,25 +127,44 @@ namespace SGS_TEST_SYSTEM_ConsoleAPP
             if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
             {
                 var value = xlRange.Cells[i, j].Value2.ToString() + "\t";
-                
+
                 Console.Write(value);
                 loopCount++;
                 Console.WriteLine(loopCount);
             }
-            if (i <= rowCount) {
-               
+            if (i <= rowCount)
+            {
+
                 SheetReturValues["i"] = Convert.ToString(i);
-              
-                while(j <= colCount)
+
+                while (j <= colCount)
                 {
                     j++;
                     SheetReturValues["j"] = Convert.ToString(j);
                     ExelSheetValues(j, i);
                 }
             }
-            
+
             return SheetReturValues;
 
         }
+
+        public static string SwitchPress()
+        {
+            bool Flag = true;
+            while (Flag)
+            {
+
+                var readText = Console.ReadLine();
+                if (readText != "")
+                {
+                    return readText;
+
+                }
+
+            }
+            return string.Empty;
+        }
     }
+
 }
