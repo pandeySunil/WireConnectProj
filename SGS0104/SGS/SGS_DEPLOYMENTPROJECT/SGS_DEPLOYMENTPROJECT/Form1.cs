7c3055f7be2af688ee86636b648dfdbbed502a12
Sunil.Pandey <Sunil.Pandey@DESKTOP-M1K358G>
2019-07-24 09:50:34 +0530

    Image Editor added

--- SGS0104/SGS/SGS_DEPLOYMENTPROJECT/SGS_DEPLOYMENTPROJECT/Form1.cs
+++ SGS0104/SGS/SGS_DEPLOYMENTPROJECT/SGS_DEPLOYMENTPROJECT/Form1.cs
@@ -18,8 +18,10 @@ namespace SGS_DEPLOYMENTPROJECT
     public partial class Form1 : Form
     {
         public bool backgroudThreadSleepFlag;
+        public bool imageBlickFalg = true;
         BusinessLogic businessLogic { get; set; }
         public static SerialPort SerialPort;
+        public  ImageGetter ImageGetter = new ImageGetter();
         Microsoft.Office.Interop.Excel.Range xlRange;
         public static int loopCount = 1;
         public static string ledOnCode;
@@ -66,7 +68,8 @@ namespace SGS_DEPLOYMENTPROJECT
                 MessageBox.Show("Please Select  Sheet To Be Loaded");
             }
             //SelectExcelFile();
-
+           var  ImageLoadThread = new Thread(() => LoadImage());
+            ImageLoadThread.IsBackground = true;
             BackGroundThread = new Thread(() => Navigation());
             BackGroundThread.IsBackground = true;
             //SerialPort = ArdiunoConnection.IntializeAudiun();
@@ -74,12 +77,12 @@ namespace SGS_DEPLOYMENTPROJECT
             //SerialPort.Open();
             businessLogic = new BusinessLogic();
             //Helper.ExcelSheetName = "";
-           
-            
-           // textBoxDevelopersArea.Text += "Sheet Intiallized\\n";
-            // pictureBox.Dock = DockStyle.Fill;
-            
 
+
+            // textBoxDevelopersArea.Text += "Sheet Intiallized\\n";
+            // pictureBox.Dock = DockStyle.Fill;
+
+            ImageLoadThread.Start();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -101,17 +104,18 @@ namespace SGS_DEPLOYMENTPROJECT
 
         private void button5_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = MessageBox.Show("LogOut will Close the Application Do you Wish To Continue ", "LogOut", MessageBoxButtons.YesNo);
-            if (dialogResult == DialogResult.Yes)
-            {
-                BackGroundThread.Abort();
-                Application.Exit();
-                LogOut();
-            }
-            else if (dialogResult == DialogResult.No)
-            {
-                return;
-            }
+            Application.Exit();
+           // DialogResult dialogResult = MessageBox.Show("LogOut will Close the Application Do you Wish To Continue ", "LogOut", MessageBoxButtons.YesNo);
+            //if (dialogResult == DialogResult.Yes)
+            //{
+            //    BackGroundThread.Abort();
+            //    Application.Exit();
+            //    LogOut();
+            //}
+            //else if (dialogResult == DialogResult.No)
+            //{
+            //    return;
+            //}
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -192,18 +196,35 @@ namespace SGS_DEPLOYMENTPROJECT
 
                             temp2 = xlRange.Cells[i, 5].Value2.ToString().Trim('/');
                             ledOnCode = trayCode + temp1;
-                           // textBoxDevelopersArea.Text += "ledOnCode: " + ledOnCode + "\\n";
+                            // textBoxDevelopersArea.Text += "ledOnCode: " + ledOnCode + "\\n";
                             Console.WriteLine(ledOnCode);
                             SerialPort.Write(ledOnCode);
                             textBoxDescription.Text = xlRange.Cells[i, 7].Value2.ToString().Trim('/');
-                           // textBoxDevelopersArea.Text += "DesMag: " + xlRange.Cells[i, 7].Value2.ToString().Trim('/') + "\\n";
+                            // textBoxDevelopersArea.Text += "DesMag: " + xlRange.Cells[i, 7].Value2.ToString().Trim('/') + "\\n";
                             textBoxPullConnection.Text = xlRange.Cells[i, 8].Value2.ToString().Trim('/');
 
                             imgUrl = xlRange.Cells[i, 9].Value2.ToString().Trim('/');
                             Bitmap image = new Bitmap("C:\\Users\\Akshay\\Desktop\\SGS\\SGS_MEDIA\\" + imgUrl);
-                            pictureBox.Image = (Image)image;
+                           // pictureBox.Image = (Image)image;
+                            var originalImage = (Image)ImageGetter.GetBitmap(0);
+                            var modifiedImage = (Image)ImageGetter.GetBitmap(1);
+                            bool toggleFlag = false;
+                            var imageId = 1;
+                            while (imageBlickFalg)
+                            {
+                                if (toggleFlag)
+                                {
+                                    pictureBox.Image = originalImage;
+                                    toggleFlag = false;
+                                }
+                                else
+                                {
+                                    pictureBox.Image = modifiedImage;
+                                    toggleFlag = true;
+                                }
+                                Thread.Sleep(250);
+                            }
                         }
-
 
                     }
 
@@ -363,6 +384,30 @@ namespace SGS_DEPLOYMENTPROJECT
         {
             labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
 
+        }
+        public void LoadImage() {
+
+            var originalImage = (Image)ImageGetter.GetBitmap(0);
+            var modifiedImage = (Image)ImageGetter.GetBitmap(1);
+            bool toggleFlag = false;
+            var imageId = 1;
+            while (imageBlickFalg)
+            {
+                if (toggleFlag)
+                {
+                  
+                    pictureBox.Image = originalImage;
+                    toggleFlag = false;
+                    
+                }
+                else
+                {
+                  
+                    pictureBox.Image = modifiedImage;
+                    toggleFlag = true;
+                }
+                Thread.Sleep(250);
+            }
         }
     }
 }
---
+++ SGS0104/SGS/SGS_DEPLOYMENTPROJECT/SGS_DEPLOYMENTPROJECT/ImageGetter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SGS_DEPLOYMENTPROJECT
+{
+  public  class ImageGetter
+    {
+        public string filePath { get; set; }
+        public Image image { get; set; }
+        public Bitmap pbImageBitmap { get; set; }
+        public List<JsonShape> jsonShapes;
+        public List<shape> drawnShapes = new List<shape>();
+        public ImageGetter() {
+
+            try
+            {
+
+                using (StreamReader r = new StreamReader(@"C:\Users\Sunil.Pandey\Desktop\CustmImageEditor\CustmImageEditor\ImageConfig.json"))
+                {
+                    Console.WriteLine("Reading File..");
+                    string json = r.ReadToEnd();
+                    Console.WriteLine("Reading File Done");
+                    jsonShapes = JsonConvert.DeserializeObject<List<JsonShape>>(json);
+
+
+
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+
+            }
+            foreach (var r in jsonShapes)
+            {
+                Pen p = new Pen(Color.Green);
+                if (r.penColor == "White")
+                {
+                    p = new Pen(Color.White);
+                }
+                else if (r.penColor == "Black")
+                {
+                    p = new Pen(Color.Black);
+                }
+                else if (r.penColor == "Yellow")
+                {
+                    p = new Pen(Color.Yellow);
+                }
+                else if (r.penColor == "Red")
+                {
+                    p = new Pen(Color.Red);
+                }
+                drawnShapes.Add(new shape()
+                {
+                    x_cortinate = r.x_cortinate,
+                    y_cortinate = r.y_cortinate,
+                    penSize = r.penSize,
+                    reatSize = r.reatSize,
+                    reactString = r.reactString,
+                    p = p
+
+                });
+
+            }
+
+        }
+        public Bitmap GetBitmap(int shapepointer) {
+            filePath = "C:\\Users\\Sunil.Pandey\\Desktop\\Capture2.PNG";
+            if (shapepointer == 0)
+            {
+
+                return new Bitmap(filePath);
+            }
+            else
+            {
+               
+                image = Image.FromFile(filePath);
+                DrawShape(drawnShapes[shapepointer].x_cortinate, drawnShapes[shapepointer].y_cortinate,
+                    drawnShapes[shapepointer].reatSize, drawnShapes[shapepointer].p, drawnShapes[shapepointer].penSize, drawnShapes[shapepointer].reactString
+                    );
+                return pbImageBitmap;
+            }
+        }
+        private void DrawShape(int x, int y, int reacSize, Pen pen, int penSize, string reactString)
+        {
+             pbImageBitmap = (Bitmap)(image);
+          var   graphics = Graphics.FromImage((Image)pbImageBitmap);
+            Rectangle rect = new Rectangle(x, y, reacSize, reacSize);
+            pen.Width = penSize;
+            graphics.DrawRectangle(pen, rect);
+          //  pictureBox1.Refresh();
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(pen.Color);
+            Font font = new Font("Times New Roman", 12.0f);
+            graphics.DrawString(reactString, font, myBrush, x, y);
+
+
+        }
+    }
+    public class RectColor
+    {
+        public Pen p { get; set; }
+        public string ColorName { get; set; }
+
+    }
+    public class JsonRectColor
+    {
+        public string ColorName { get; set; }
+    }
+    public class RectSize
+    {
+        public int size { get; set; }
+
+
+    }
+    public class PenSize
+    {
+        public int size { get; set; }
+
+    }
+    public class RootObject
+    {
+        List<JsonShape> jsonShapes { get; set; }
+    }
+    public class JsonShape
+    {
+        public int Id { get; set; }
+        public int x_cortinate { get; set; }
+        public int y_cortinate { get; set; }
+        public int penSize { get; set; }
+        public int reatSize { get; set; }
+        public string penColor { get; set; }
+        public string reactString { get; set; }
+
+    }
+    public class shape
+    {
+        public int Id { get; set; }
+        public int x_cortinate { get; set; }
+        public int y_cortinate { get; set; }
+        public int penSize { get; set; }
+        public int reatSize { get; set; }
+        public Pen p { get; set; }
+        public string reactString { get; set; }
+
+    }
+}
